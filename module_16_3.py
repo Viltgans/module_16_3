0791382c102b201290b5ea6448543c155ff8cb45
Viltgans <80921674+Viltgans@users.noreply.github.com>
2024-12-09 16:09:13 +0300

    Add files via upload

---
+++ module_16_3.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+users = {'1': 'Имя: Example, возраст: 18'}
+
+@app.get("/")
+async def read_root():
+    return 'Главная страница'
+
+@app.get("/users")
+async def users_list() -> dict:
+    return users
+
+@app.post('/user/{username}/{age}')
+async def create_user(username: str, age: int) -> str:
+    user_id = str(int(max(users, key=int)) + 1)
+    users[user_id] = f'Имя: {username}, возраст: {age}'
+    return f"User {user_id} is registered"
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user(user_id: str, username: str, age: int) -> str:
+    users[user_id] = f'Имя: {username}, возраст: {age}'
+    return f"User {user_id} has been updated"
+
+@app.delete('/user/{user_id}')
+async def delete_user(user_id: str) -> str:
+    users.pop(user_id)
+    return f'User {user_id} has been deleted'
+
+## Запуск:
+## 1. Переход в директорию: cd module_16/homework_16_3/
+## 2. Сам запуск: uvicorn module_16_3:app --reload
