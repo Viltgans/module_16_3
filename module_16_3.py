a72d460fbb1a0ee01393a7f97befbfecadb7d872
Viltgans <80921674+Viltgans@users.noreply.github.com>
2024-12-12 07:37:50 +0300

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -17,13 +17,15 @@ async def create_user(username: Annotated[str, Path(min_length=5, max_length=20,
     return f"User {user_id} is registered"
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: str, username: str, age: int) -> str:
+async def update_user(user_id: Annotated[int, Path(gt=0, lt=100, description='Enter User ID', example='1')],
+                      username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+                    age: Annotated[int, Path(le=120, ge=18, description='Enter age', example='24')]) -> str:
     users[user_id] = f'Имя: {username}, возраст: {age}'
     return f"User {user_id} has been updated"
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: str) -> str:
-    users.pop(user_id)
+async def delete_user(user_id: int = Path(gt=1, lt=100, description='Enter User ID', example='1')) -> str:
+    users.pop(str(user_id))
     return f'User {user_id} has been deleted'
 
 ## Запуск:
